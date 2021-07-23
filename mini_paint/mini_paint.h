6d5b6151dd39527e337d386400fd0f413da1f65d
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-07-23 19:46:58 +0300

    Add files via upload

--- mini_paint/mini_paint.h
+++ mini_paint/mini_paint.h
@@ -41,14 +41,4 @@ typedef struct s_img
 	char	**image;
 } t_img;
 
-int			ft_strlen(const char *s);
-int			err_message(char *error);
-int 		free_all(FILE *file, t_img *img);
-char 		**fill_cnvs(FILE *file, t_img *img);
-int			is_in_circl(float x, float y, t_img *img);
-void		fill_fgr_2(t_img *img);
-int			fill_fgr_1(FILE *file, t_img *img);
-void		print_image(t_img *img);
-int 		main(int argc, char **argv);
-
 #endif
