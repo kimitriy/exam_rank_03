20b1fbd46c26d9b63e4374e3515c203d0a277105
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-07-23 19:46:17 +0300

    Add files via upload

--- my_ceil_floor.c
+++ my_ceil_floor.c
@@ -27,24 +27,28 @@ int		round_floor(float n)
 	int		i;
 
 	sign = 1;
+	i = 0;
 	if (n < 0)
 	{
 		sign = -1;
 		n *= -1;
+		while (i <= n)
+			i++;
 	}
-	i = 0;
-	while (i <= n)
-		i++;
-	i--;
+	else
+	{
+		while ((n - i) > 1)
+			i++;
+	}
 	return (sign * i);
 }
 
 int		 main()
 {
-	float f = -4.455;
+	float f = 0.500;
 
 	printf("my_ceil_f: %d\n", round_ceil(f));
-	printf("real_ceil_f: %d\n", (int)ceil(f));
+	printf("real_ceil_f: %d\n", (int)ceilf(f));
 	printf("my_floor_f: %d\n", round_floor(f));
-	printf("real_floor_f: %d\n", (int)floor(f));
+	printf("real_floor_f: %d\n", (int)floorf(f));
 }
